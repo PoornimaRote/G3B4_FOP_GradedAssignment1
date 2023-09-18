d0a5763d8cd543177296211eba18607e7b9cc3c6
PoornimaRote <142153202+PoornimaRote@users.noreply.github.com>
2023-09-18 18:41:22 +0530

    Add files via upload

---
+++ Department/src/com/greatLearning/department/Super/SuperDepartment.java
@@ -0,0 +1,31 @@
+package com.greatLearning.department.Super;
+
+public class SuperDepartment implements IDepartment {
+
+	@Override
+	public String departmentName() {
+		
+		return "Super Department";
+	}
+
+	@Override
+	public String getTodaysWork() {
+		
+		return "No work as of now";
+	}
+
+	@Override
+	public String getWorkDeadline() {
+		
+		return "Nil";
+	}
+
+	@Override
+	public String isTodayAHoliday() {
+		
+		return "Today is not a Holiday";
+	}
+	
+	
+
+}
